1446dd57c0d95c736f9598eeefb4f6efd6df08c3
slimshady7111 <iniorg472@gmail.com>
2025-07-19 15:09:35 +0700

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,7 @@
-
+export default function Home() {
+  return (
+    <main className="flex h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">Welcome to BaikHaty+</h1>
+    </main>
+  );
+}
